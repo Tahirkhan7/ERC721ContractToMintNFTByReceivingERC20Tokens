fbd533dcfcd57800116427615c838cdd40f2e48a
Tahir Khan <44526177+Tahirkhan7@users.noreply.github.com>
2022-03-17 21:55:30 +0530

    Update main.sol

--- contracts/main.sol
+++ contracts/main.sol
@@ -7,13 +7,9 @@ contract Token {
 
     t1 test1;
     t2 test2;
-    // erc test2;
+
     testERC721 erc721;
-    // constructor(address ERC,address token1,address token2){
-    //     erctest=testERC721(ERC);
-    //     test1=erc(token1);
-    //     test2=erc(token2);
-    // }
+
 constructor(address token1,address token2,address erctest){
         test1=t1(token1);
         test2=t2(token2);
@@ -40,12 +36,3 @@ constructor(address token1,address token2,address erctest){
 }
 
 
-
-//steps to run this project
-//1) Deploy erc contract in myerc20.sol file with token1 name,symbol, fee receiver.
-//2) Repeat the step 2 to create token2.
-//3) Deploy the TestERC721 contract in new3.sol file.
-//4) Deploy the Token contract in new5.sol file with address of ERC721, token1, token2 contract address in constructor.
-//5) Copy Token contract address and paste it in transferOwnership function of TestERC721 contract and run.
-//6) Now run mintMeERC721 function of Token contract with TokenURI, Amount of test Token1, Token2. run
-//7)Done!!
