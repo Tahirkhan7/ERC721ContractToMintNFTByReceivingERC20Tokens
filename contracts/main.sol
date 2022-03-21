6bfdd5cd99e16613ef4adc5a5c6267ddcb6d933b
Tahir Khan <44526177+Tahirkhan7@users.noreply.github.com>
2022-03-21 23:03:12 +0530

    Update main.sol

--- contracts/main.sol
+++ contracts/main.sol
@@ -16,17 +16,15 @@ constructor(address token1,address token2,address erctest){
         erc721=testERC721(erctest);
     }
 
-    function mintMeERC721(string memory tokenURI)  public {
+    function mintMeERC721(string memory tokenURI,uint amountToken1, uint amountToken2)  public {
         require(amountToken1>=100,"Test1 Token need to be greater than or equals to 100!");
         require(amountToken2>=200,"Test1 Token need to be greater than or equals to 200!");
         ERC20(test1).approve(address(this),amountToken1);
         ERC20(test1).transferFrom(msg.sender, address(this), amountToken1);
-        test1.mint(address(this),amountToken1);
-        ERC20(test2).approve(address(this),amountToken1);
-        ERC20(test2).transferFrom(msg.sender, address(this), amountToken1);
-        test2.mint(address(this),amountToken2);
+        test1.mintToken1(address(this),amountToken1);
+        ERC20(test2).approve(address(this),amountToken2);
+        ERC20(test2).transferFrom(msg.sender, address(this), amountToken2);
+        test2.mintToken2(address(this),amountToken2);
         erc721.sendERC721(address(this),msg.sender,tokenURI);
     }
 }
-
-
