5faa453458a08cc615bf561cc50ea956e8f9c242
Tahir Khan <44526177+Tahirkhan7@users.noreply.github.com>
2022-03-21 23:05:58 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -34,23 +34,38 @@ describe("Token Contract", function () {
 
 
   describe("Transactions", function () {
-    it("Should fail if sender does not have atleast 100 test1 tokens", async function () {
+    it("Should fail if sender does not have atleast 100 test1 tokens.", async function () {
       //owner account to token1.address
+      const approved= await tok1.approve(m,100000000);
       const ownerToken1 = await tok1.transferOwnership(m);
-      await mn.mintMeToken1(100);
+      const approved2= await tok2.approve(m,100000000);
+      const ownerToken2 = await tok2.transferOwnership(m);
+      const ownerToken3 = await ERC721nft.transferOwnership(m);
+      await mn.mintMeERC721("tk",100,200);
 
     });
 
-    it("Should fail if sender does not have atleast 200 test2 tokens", async function () {
+    it("Should fail if sender does not have atleast 200 test2 tokens.", async function () {
         //owner account to token1.address
-        const ownerToken1 = await tok2.transferOwnership(m);
-        await  mn.mintMeToken2(200);
+        const approved= await tok1.approve(m,100000000);
+        const ownerToken1 = await tok1.transferOwnership(m);
+        const approved2= await tok2.approve(m,100000000);
+        const ownerToken2 = await tok2.transferOwnership(m);
+        const ownerToken3 = await ERC721nft.transferOwnership(m);
+        await mn.mintMeERC721("tk",100,200);
   
       });
 
-      it("Should fail if caller is not the owner of the contract", async function () {
+      it("Should fail if caller is not the owner of the contract.", async function () {
         //owner account to token1.address
         const ownerToken1 = await tok2.transferOwnership(m);
+  
+      });
+
+      it("Should fail if caller didn't get approved to transfer tokens into its.", async function () {
+        //owner account to token1.address
+        await expect(  tok1.approve(m,10000000)
+        ).to.be.revertedWith("Not enough tokens");;
   
       });
 
