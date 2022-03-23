7c8b93c9733d22e0e802e9d9d1b26a318700b278
Tahir Khan <44526177+Tahirkhan7@users.noreply.github.com>
2022-03-23 18:18:01 +0530

    Update main.sol

--- contracts/main.sol
+++ contracts/main.sol
@@ -1,22 +1,26 @@
 pragma solidity ^0.8.0;
-import "./erc721.sol";
 import "./token1.sol";
 import "./token2.sol";
 
-contract Token {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+
+contract Token is ERC721URIStorage, Ownable{
 
     t1 test1;
     t2 test2;
 
-    testERC721 erc721;
 
-constructor(address token1,address token2,address erctest){
+constructor(address token1,address token2,address erctest) ERC721("Robot NFT", "ROB"){
         test1=t1(token1);
         test2=t2(token2);
-        erc721=testERC721(erctest);
     }
 
-    function mintMeERC721(string memory tokenURI,uint amountToken1, uint amountToken2)  public {
+    function mintMeERC721(string memory tokenURI,uint amountToken1, uint amountToken2)  public returns (uint){
         require(amountToken1>=100,"Test1 Token need to be greater than or equals to 100!");
         require(amountToken2>=200,"Test1 Token need to be greater than or equals to 200!");
         require(
@@ -31,6 +35,14 @@ constructor(address token1,address token2,address erctest){
         require(sent1,"Token 1 transfer failed!");
         bool sent2=ERC20(test2).transferFrom(msg.sender, address(this), amountToken2);
         require(sent2, "Token 2 transfer failed");
-        erc721.sendERC721(address(this),msg.sender,tokenURI);
+        
+        
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
     }
 }
